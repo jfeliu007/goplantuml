cd08f181b53c9aa762c929ed81fd95da565ca871
Javier Feliu <javier.feliu@creativedrive.com>
2019-10-22 16:21:19 -0400

    for issue 45. GoLint comments

--- testingsupport/subfolder2/subfolder2.go
+++ testingsupport/subfolder2/subfolder2.go
@@ -1,9 +1,10 @@
 package subfolder2
 
+//Subfolder2 structure for testing purpose only
 type Subfolder2 struct {
 }
 
-//SubfolferFunction for testing purposes
+//SubfolderFunction for testing purposes
 func (s *Subfolder2) SubfolderFunction(bool, int) bool {
 	return true
 }
