d8f17225790c6dec1af916fc9eb1f3558ab58f9a
Javier Feliu <javier.feliu@creativedrive.com>
2019-10-25 23:40:25 -0400

    forgot to add new tests for connection-labels

---
+++ testingsupport/connectionlabels/connectionlabels.go
@@ -0,0 +1,19 @@
+package connectionlabels
+
+//AbstractInterface for testing purposes
+type AbstractInterface interface {
+	interfaceFunction() bool
+}
+
+//ImplementsAbstractInterface for testing purposes
+type ImplementsAbstractInterface struct {
+	AliasOfInt
+	PublicUse AbstractInterface
+}
+
+func (iai *ImplementsAbstractInterface) interfaceFunction() bool {
+	return true
+}
+
+//AliasOfInt for testing purposes
+type AliasOfInt int
