c32efbef83fe5941ff77092317eb0660d5d1737b
Javier Feliu <jfeliu@honorlock.com>
2022-04-04 09:30:51 -0400

    Fix return parametrized error and added go Mod.
    Return code renderer was missing the loop to add multiple instances of the same type if there were multiple return named values with the same type in a list form.
    I also added support for go mod (Finally!) fixes #127 fixed #122 fixes #31

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/jfeliu007/goplantuml
+
+go 1.17
+
+require github.com/spf13/afero v1.8.2
+
+require golang.org/x/text v0.3.7 // indirect
--- testingsupport/subfolder2/subfolder2.go
+++ testingsupport/subfolder2/subfolder2.go
@@ -8,3 +8,7 @@ type Subfolder2 struct {
 func (s *Subfolder2) SubfolderFunction(b bool, i int) bool {
 	return true
 }
+
+func (s *Subfolder2) SubfolderFunctionWithReturnListParametrized() (a, b, c []byte, err error) {
+	return
+}
